d5a3d9e70d16d5c97c05aad904a61209cd57b229
Antony Chris <36199509+chrisalbey7@users.noreply.github.com>
2022-06-01 12:34:38 -0400

    commit 2

--- app.py
+++ app.py
@@ -38,18 +38,18 @@ def story():
     print('There once was a ', character_age , " year old ", age_group, "named", character_name, ",")
     print(character_name + " likes " + character_flavour + " icecream.")
     print(character_name + " grew strong by eating " + character_flavour + " icecream, until one day, a strange thing happened!!")
-    print(character_name+"'s village " + character_place+ " ran out of all the "+ character_flavour + " icecreams")
+    print(character_name+"\'s village " + character_place+ " ran out of all the "+ character_flavour + " icecreams")
 
 # Ready to play?
 def main():
     print('')
-    enter = str(input("DO you want to begin this adventure? (Y/N)"))
-    if enter ==  "Y" :
+    enter = str(input("Do you want to begin this adventure? (Y/N)"))
+    if enter ==  "Y"  or "y":
         return game_mode()
     elif enter == "N" :
         print("OK tell me when you're ready")
         enter = input("Are you ready? ")
-    if enter ==  "Y" :
+    if enter ==  "Y"  or "y":
         return game_mode()
     else: input("Wrong input try again")
 
@@ -61,7 +61,7 @@ def game():
 #Excited to play?
 def game_mode():
     z = input("Let's collect all the ice creams!! : (Y/N)")
-    if z == "Y":
+    if z == "Y" or "y":
      return game()
     else:
         print("Wrong input... Try again!")
