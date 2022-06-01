639d380d1a071f1bfde04d658dc6377907914219
Antony Chris <36199509+chrisalbey7@users.noreply.github.com>
2022-06-01 12:53:18 -0400

    commit 2

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ line
 comment
 """
 
-
 print("Hello World!")
 
 # Get user input, init variables
@@ -22,7 +21,7 @@ character_place = character_place.capitalize()
 character_flavour = input("Which is your favourate icecream flavour? ")
 
 # checks the age and assigns a bool value to y (comparison operator) to assign it to a specefic age group
-y = ( character_age <= 19 and character_age >= 10)
+y = (character_age <= 19 and character_age >= 10)
 if y == True:
     age_group = "boy"
 elif character_age <= 10:
@@ -30,41 +29,54 @@ elif character_age <= 10:
 else:
     age_group = "man"
 
+
 # Function that describes the intro story
 def story():
     print('')
     print("Story")
     print('')
-    print('There once was a ', character_age , " year old ", age_group, "named", character_name, ",")
+    print('There once was a ', character_age, " year old ", age_group, "named", character_name, ",")
     print(character_name + " likes " + character_flavour + " icecream.")
-    print(character_name + " grew strong by eating " + character_flavour + " icecream, until one day, a strange thing happened!!")
-    print(character_name+"\'s village " + character_place+ " ran out of all the "+ character_flavour + " icecreams")
+    print(
+        character_name + " grew strong by eating " + character_flavour + " icecream, until one day, a strange thing happened!!")
+    print(character_name + "\'s village " + character_place + " ran out of all the " + character_flavour + " icecreams")
+
 
 # Ready to play?
 def main():
     print('')
     enter = str(input("Do you want to begin this adventure? (Y/N)"))
-    if enter ==  "Y"  or "y":
+    if enter == "Y":
         return game_mode()
-    elif enter == "N" :
+    elif enter == "y":
+        return game_mode()
+    else:
         print("OK tell me when you're ready")
-        enter = input("Are you ready? ")
-    if enter ==  "Y"  or "y":
+        enter2 = input("Are you ready? ")
+    if enter2 == "Y":
         return game_mode()
-    else: input("Wrong input try again")
+    elif enter2 == "y":
+        return game_mode()
+    else:
+        input("Wrong input try again")
+
 
 # Surprise!
 def game():
     print('')
     print("Hello gamer Fuck you! find some real games bruhh...! :) ")
 
-#Excited to play?
+
+# Excited to play?
 def game_mode():
     z = input("Let's collect all the ice creams!! : (Y/N)")
-    if z == "Y" or "y":
-     return game()
+    if z == "Y":
+        return game()
+    elif z == "y":
+        return game()
     else:
-        print("Wrong input... Try again!")
+        print('Wrong input... Try again!')
+
 
 # Here goes my First application
 story()
